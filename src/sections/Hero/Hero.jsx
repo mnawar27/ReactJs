6fac17f039bad7d50fa124897413c7255e4aae0a
Maisha Nawar <mnawar@Maishas-MacBook-Pro.local>
2024-12-03 00:32:32 -0500

    fixed some codes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -7,7 +7,7 @@ import githubLight from "../../assets/github-light.svg"
 import githubDark from "../../assets/github-dark.svg"
 import linkedinLight from "../../assets/linkedin-light.svg"
 import linkedinDark from "../../assets/linkedin-dark.svg"
-import RSM from '../../assets/rsm.pdf'
+import RSM from '../../assets/Maisha_Resume.pdf'
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
